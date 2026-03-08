3536aeffd2b4a11fbab049fdf28ea5020df59a6b
Thomas Mouron <thomas.mouron@swile.co>
2026-03-08 20:15:03 +0100

    seo improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://santaix.fr',
   base: '/',
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
